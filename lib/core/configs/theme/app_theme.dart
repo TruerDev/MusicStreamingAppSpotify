a4239169aa50a842e9a3c4271b51c6484be9ed2d
Truer <truuuuer@gmail.com>
2025-01-19 01:24:50 +0300

    Initial commit

---
+++ lib/core/configs/theme/app_theme.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_clone_app/core/configs/theme/app_colors.dart';
+
+class AppTheme {
+
+  static final lightTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.lightBG,
+    brightness: Brightness.light,
+    fontFamily: 'Satoshi',
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        elevation: 0,
+        textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30)
+        )
+      )
+    )
+  );
+
+  static final darkTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.darkBG,
+    brightness: Brightness.dark,
+    fontFamily: 'Satoshi',
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30)
+        )
+      )
+    )
+  );
+}
---
+++ lib/presentation/choose_mode/page/choose_mode.dart
@@ -0,0 +1,157 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
+
+import 'package:spotify_clone_app/common/widgets/button/basic_app_button.dart';
+import 'package:spotify_clone_app/core/configs/assets/app_images.dart';
+import 'package:spotify_clone_app/core/configs/assets/app_vectors.dart';
+import 'package:spotify_clone_app/core/configs/theme/app_colors.dart';
+import 'package:spotify_clone_app/presentation/choose_mode/bloc/theme_cubit.dart';
+
+class ChooseModePage extends StatefulWidget {
+  const ChooseModePage({super.key});
+
+  @override
+  _ChooseModePageState createState() => _ChooseModePageState();
+}
+
+class _ChooseModePageState extends State<ChooseModePage> {
+  String? _selectedMode;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedMode = 'Dark mode';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 40,
+              horizontal: 40,
+            ),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(
+                  AppImages.chooseModeBG,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.15),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(
+                    AppVectors.logo,
+                  ),
+                ),
+                const Spacer(),
+                const Text(
+                  'Choose Mode',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(height: 40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _buildModeOption(
+                      context,
+                      mode: 'Dark mode',
+                      icon: AppVectors.moon,
+                      onTap: () {
+                        setState(() {
+                          _selectedMode = 'Dark mode';
+                        });
+                        context.read<ThemeCubit>().updateTheme(ThemeMode.dark);
+                      },
+                    ),
+                    const SizedBox(width: 60),
+                    _buildModeOption(
+                      context,
+                      mode: 'Light mode',
+                      icon: AppVectors.sun,
+                      onTap: () {
+                        setState(() {
+                          _selectedMode = 'Light mode';
+                        });
+                        context.read<ThemeCubit>().updateTheme(ThemeMode.light);
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 120),
+                BasicAppButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => const ChooseModePage(),
+                      ),
+                    );
+                  },
+                  title: 'Continue',
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildModeOption(BuildContext context,
+      {required String mode, required String icon, required VoidCallback onTap}) {
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: onTap,
+          child: ClipOval(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              child: Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                  color: _selectedMode == mode
+                      ? Colors.green.withOpacity(0.6)
+                      : const Color(0xff30393C).withOpacity(0.4),
+                  shape: BoxShape.circle,
+                ),
+                child: SvgPicture.asset(
+                  icon,
+                  fit: BoxFit.none,
+                ),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 15),
+        Text(
+          mode,
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 17,
+            color: _selectedMode == mode ? Colors.green : AppColors.grey,
+          ),
+        ),
+      ],
+    );
+  }
+}
